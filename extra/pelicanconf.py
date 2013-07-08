036d179768a24b9c7e89292789ebbb66ab16448d
bussiere <bussiere@gmail.com>
2013-07-08 09:12:45 +0200

    plugins

--- extra/pelicanconf.py
+++ extra/pelicanconf.py
@@ -9,6 +9,7 @@ TIMEZONE = 'Europe/Paris'
 FEED_DOMAIN = 'http://bussiere.github.com/RapidNews'
 DEFAULT_LANG = u'en'
 
+
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
@@ -19,5 +20,21 @@ SOCIAL = (('Bussiere', 'http://bussiere.github.com/'),)
 DEFAULT_PAGINATION = 10
 
 THEME = "../pelican-themes/bootstrap2"
-
+PLUGIN_PATH = "../pelican-plugins/"
+PLUGINS = ['sitemap',]
+FEED_MAX_ITEMS = 5
 FEED_ATOM = 'feeds/all.atom.xml'
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'daily',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
